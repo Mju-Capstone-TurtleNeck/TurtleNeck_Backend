6b666743e305b560494543ed44d23bb076a36603
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-31 13:55:13 +0900

    Feat: Implement auth feature

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
+  //쿠키에서 토큰 정보를 가져옴
   let token = req.cookies.w_auth;
 
   User.findByToken(token, (err, user) => {
     if (err) throw err;
+    //해당 토큰을 가진 유저가 존재하지 않으면, 인증 실패
     if (!user)
-      return res.json({
-        isAuth: false,
-        error: true
-      });
-
+      return res.json({isAuth: false, err:true, message:'인증 실패'});
+    
+    //인증성공
     req.token = token;
     req.user = user;
     next();
